593aa7865639402e2e3e21a6cc4d22ca0aeb8caa
Asif Zaman <69311703+AsifZaman777@users.noreply.github.com>
2021-06-10 16:27:48 +0600

    Delete Python Tutorial/Py15 directory

--- Python Tutorial/Py15/Directory handling/Directory deletion.py
+++
@@ -1,7 +0,0 @@
-import shutil
-try:
-   path="C:\\Users\\User\\Desktop\\PyFile"  # path of the directory
-   shutil.rmtree(path)  #remove the whole directory"
-   print("Directory deleted")
-except:
-    print("Directory doesn't exist")
--- Python Tutorial/Py15/File Handling/File deletion.py
+++
@@ -1,4 +0,0 @@
-import os
-import shutil
-os.remove("C:\\Users\\User\\Desktop\\PyFile\\HalalBoys.txt")  # it will delete the file in particular directory
-
